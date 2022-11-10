7be20608b7c71bf41c0cba4c5e100c1e20442499
renren-017 <altynai.mamytova17@gmail.com>
2022-11-10 18:40:14 +0600

    Steps 1-5

---
+++ content_aggregator/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from content_aggregator.users.models import User
+from content_aggregator.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
